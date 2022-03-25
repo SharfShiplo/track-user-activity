d845e48564c4020a1e0c28caf438b02905479104
SharfShiplo <sharf.ahammed@gmail.com>
2022-03-25 10:51:46 +0600

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,31 @@
-import logo from './logo.svg';
+import { useEffect, useState } from 'react';
 import './App.css';
+import IdleTimer from './idle-timer';
 
 function App() {
+  const [isTimeout, setIsTimeout] = useState(false);
+
+  useEffect(() =>{
+    const timer = new IdleTimer({
+      timeout: 15, 
+      onTimeout: () => {
+        setIsTimeout(true);
+      },
+      onExpired : () => {
+        setIsTimeout(true);
+      }
+    });
+
+    return ()=> {
+      timer.cleanUp();
+    }
+  }, [])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <section className="App-header" aria-label='app header'>
+      <h1>{isTimeout ? "Inactive" : "Active"}</h1>
+      <small>If you do not have any activity on mouse or keyboard for 15s it will consider you as inactive.</small>
+    </section>
   );
 }
 
